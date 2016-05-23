84649ae90a9a26eb0735626862e8c482f83a0eb3
RivervdMeulen <River@Rivers-MacBook-Pro.local>
2016-05-23 08:10:02 +0200

    fixed radar bug

---
+++ Assets/Scripts/RadarNeedle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RadarNeedle : MonoBehaviour {
+
+	[SerializeField]
+	private RectTransform Needle;
+
+	[SerializeField]
+	private float rotateSpeed;
+
+	private float rotation = -90f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Needle.rotation = Quaternion.Euler (0, 0, rotation);
+		rotation += rotateSpeed;
+	}
+}
